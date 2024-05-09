c6e3400529c7c9117d9d4a1d7e45424843b5c92f
Sumit5145 <162263489+Sumit5145@users.noreply.github.com>
2024-05-09 16:22:48 +0530

    Create server.tf

---
+++ server.tf
@@ -0,0 +1,41 @@
+# key-pair
+
+resource "aws_key_pair" "key-tf" {
+  key_name   = "key-tf"
+  public_key = file("${path.module}/id_rsa.pub")
+}
+
+# security group
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+
+  dynamic "ingress" {
+    for_each = [22, 80, 3000, 9090, 4000]
+    iterator = port
+    content {
+      description = "TLS from VPC"
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+}
+
+# ec2 server
+
+resource "aws_instance" "web" {
+  ami                    = "ami-0f58b397bc5c1f2e8"
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.key-tf.key_name
+  vpc_security_group_ids = ["${aws_security_group.allow_tls.id}"]
+  tags = {
+    Name = "kub-master"
+  }
+}
+
+
+
+
